a45676e8b11b96900e238d2ccd5b2a48bb762720
sanjeevtalluri <sanjeevtalluri@gmail.com>
2024-04-06 10:22:46 +0530

    Added Search functionality

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,7 +5,34 @@ import { getUniqueValues, formatPrice } from '../utils/helpers'
 import { FaCheck } from 'react-icons/fa'
 
 const Filters = () => {
-  return <h4>filters</h4>
+  const { filters: {
+    text,
+    category,
+    company,
+    color,
+    min_price,
+    price,
+    max_price,
+    shipping,
+  }, updateFilters } = useFilterContext();
+  return <Wrapper>
+    <div className='content'>
+      <form onSubmit={(e) => e.preventDefault()}>
+        {/* search input */}
+        <div className='form-control'>
+          <input
+            type='text'
+            name='text'
+            value={text}
+            placeholder='search'
+            onChange={updateFilters}
+            className='search-input'
+          />
+        </div>
+        {/* end of search input */}
+      </form>
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,8 +16,17 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view:false,
-  sort:'price-lowest'
-
+  sort:'price-lowest',
+  filters:{
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  }
 };
 
 const FilterContext = React.createContext();
@@ -44,8 +53,13 @@ export const FilterProvider = ({ children }) => {
     const value = e.target.value
     dispatch({ type: UPDATE_SORT,payload:value })
   }
+  const updateFilters = (e) =>{
+    const name = e.target.name;
+    const value = e.target.value;
+    dispatch({type:UPDATE_FILTERS,payload:{name,value}});
+  }
   return (
-    <FilterContext.Provider value={{...state,setGridView,setListView,updateSort}}>
+    <FilterContext.Provider value={{...state,setGridView,setListView,updateSort, updateFilters}}>
       {children}
     </FilterContext.Provider>
   );
