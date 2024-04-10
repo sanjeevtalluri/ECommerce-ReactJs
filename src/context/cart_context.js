d34c0f01560e9d56b8307e95874beb1c9d7f5527
sanjeevtalluri <sanjeevtalluri@gmail.com>
2024-04-10 18:06:58 +0530

    Updated cart functionality

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,24 +8,40 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
-const initialState = {
-  cart:[],
-  total_items:0,
-  total_amount:0,
-  shipping_fee:534
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart')
+  if (cart) {
+    return JSON.parse(localStorage.getItem('cart'))
+  } else {
+    return []
+  }
 }
+
+const initialState = {
+  cart: getLocalStorage(),
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534
+}
+
+
 
 const CartContext = React.createContext()
 
-
-
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
-  const addToCart = (id, color, amount, product)=>{
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
+  const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   }
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  }
   return (
-    <CartContext.Provider value={{...state,addToCart}}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, addToCart,clearCart }}>{children}</CartContext.Provider>
   )
 }
 // make sure use
