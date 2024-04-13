e18589fe75f4cf9dae4c46dcc1785b54b7f7f7aa
sanjeev <sanjeevtalluri@gmail.com>
2024-04-13 10:40:09 +0530

    added stripe

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 
 root.render(
   <Auth0Provider
-    domain="dev-wiw68kzjaahrep7o.us.auth0.com"
-    clientId="h259ZEgDBAVsPQ0a3DcJnkW0DsF8YbKt"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: `${window.location.origin}`,
     }}
   >
     <UserProvider>
